ae8f7d5f17a7a33f3de2c094bf0886d2cbd0e5f8
SeonghyeonKim <kkgojn@naver.com>
2019-09-04 18:01:31 +0900

    keep

--- 10995.cpp
+++ 10995.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int print(int, int);
-
 int main(void)
 {
+	int N;
+	cin >> N;
+	
+	for(int i=0; i<N; i++) {
+		if(i%2==0) cout << " ";
+		for(int j=0; j<N; j++) {
+			
+		}
+		cout << "\n";
+	}
 	
 	return 0;
-}
-
-int print(int n, int k)
-{
-	for(int i=0; i<n; i++) {
-		cout <<"* ";
-	}
 }
