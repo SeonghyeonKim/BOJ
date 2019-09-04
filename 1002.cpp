276d289cdcb21466815b6fc5d8d892484e2f13c6
SeonghyeonKim <kkgojn@naver.com>
2019-09-04 16:49:56 +0900

    test

---
+++ 1002.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+float reach(int, int, int, int);
+
+int main()
+{
+	int x1, y1, x2, y2, r1, r2, T;
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+
+		if (x1 == x2 && y1 == y2 && r1 == r2)
+			cout << -1;
+		else if (abs(r1 - r2) < reach(x1, y1, x2, y2) && reach(x1, y1, x2, y2) < r1 + r2)
+			cout << 2;
+		else if (reach(x1, y1, x2, y2) == r1 + r2 || reach(x1, y1, x2, y2) == abs(r1 - r2))
+			cout << 1;
+		else
+			cout << 0;
+	}
+
+	return 0;
+}
+
+float reach(int x1, int y1, int x2, int y2) {
+	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
