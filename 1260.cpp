25f2b7fa39338fe6fe377b8fa85da10ce5f2dfc5
SeonghyeonKim <kkgojn@naver.com>
2019-09-24 10:39:25 +0900

    test

---
+++ 1260.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int vertex, edge, first, from, to;
+	
+	cin >> vertex >> edge >> first;
+	
+	for(int )
+	
+	
+	return 0;
+}
---
+++ 2606.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int N, T, from, to;
+	cin >> N >> T;
+	int com[N][N], virus[N]={1,};
+	
+	for(int i=0; i<T; i++) {
+		cin >> 
+	}
+	
+	return 0;
+}
+
