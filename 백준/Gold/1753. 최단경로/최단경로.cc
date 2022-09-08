43c4a37ea4527c4344e532448059e2d12e733398
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2022-09-08 15:49:03 +0900

    [Gold IV] Title: 최단경로, Time: 104 ms, Memory: 9060 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,29 +1,42 @@
+/*
+다익스트라 알고리즘을 이용한 단방향 그래프에서 
+특정한 정점에서 다른 모든 정점으로의 최단 경로 구하기 
+*/
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <vector>	// 간선을 벡터의 배열로 저장 
+#include <queue>	// 우선순위 큐 사용 
+
+#define INF 987654321 
 using namespace std;
 
+// 다익스트라 배열 
 int Dist[20001];
 
 int main() {
+	// 빠른 입출력 
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    int V, E, start;
+	// 입력 
+    int V, E, start;	// V:정점의 수, E:간선의 수, start:시작 정점 
     cin >> V >> E >> start;
 
-    vector<pair<int, int> > v[20001];
+    vector<pair<int, int> > v[20001];	// (목표 정점, 간선의 길이) 형태로 저장 
     for(int i=0; i<E; i++) {
         int a, b, c;
         cin >> a >> b >> c;
         v[a].push_back({b, c});
     }
 
-    for(int i=1; i<=V; i++) Dist[i] = 987654321;
-	Dist[start] = 0;
+	//	초기화 
+    for(int i=1; i<=V; i++) Dist[i] = INF;	// 모든 정점의 cost를 INF로 초기화 
+	Dist[start] = 0;	// 시작 정점의 cost는 0으로 초기화 
 	
-    priority_queue<pair<int, int> > pq;
-    pq.push({0, start});
+	
+	// 다익스트라 
+	// (-비용, 현재 정점), 음수의 비용을 기준으로 내림차순 
+    priority_queue<pair<int, int> > pq;	 
+    pq.push({0, start});	
 
     while(!pq.empty()) {
         int cost = -pq.top().first;
@@ -35,14 +48,15 @@ int main() {
             int nextCost = v[current][i].second;
 
             if(Dist[next] > cost + nextCost) {
-                Dist[next] = cost + nextCost;
+                Dist[next] = cost + nextCost; 
                 pq.push({-Dist[next], next});
             }
         }
     }
 
+	// 결과 출력 
     for(int i=1; i<=V; i++) {
-        if(Dist[i] == 987654321) cout << "INF\n";
+        if(Dist[i] == INF) cout << "INF\n";	// 연결되지 않음 
         else cout << Dist[i] << "\n";
     }
 
