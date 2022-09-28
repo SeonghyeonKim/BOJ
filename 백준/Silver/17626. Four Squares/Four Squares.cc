b5dd33ee5b49ebe6eb12127ccd5d72950bf8dc88
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2022-09-28 14:10:18 +0900

    [Silver III] Title: Four Squares, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.cc
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -12,21 +12,16 @@ int main(void) {
 	for(int a=0; a<=sn; a++) {
 		for(int b=a; b<=sn; b++) {
 			for(int c=b; c<=sn; c++) {
-				for(int d=c; d<=sn; d++) {
-					if(a*a+b*b+c*c+d*d==N) {
-						if(c==0) cout << 1;
-						else if(b==0) cout << 2;
-						else if(a==0) cout << 3;
-						exit(0);	
-					}	
-					if(a>0) {
-						cout << 4;
-						exit(0);
-					}
-				}
+				if(a*a+b*b+c*c==N) {
+					if(b==0) cout << 1;
+					else if(a==0) cout << 2;
+					else cout << 3;
+					exit(0);	
+				}	
 			}
 		}
 	}
+	cout << 4;
 	
 	return 0;
 }
