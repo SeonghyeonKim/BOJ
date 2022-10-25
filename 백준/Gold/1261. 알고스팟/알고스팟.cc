d374fa618aec3a893daf491a8f67019368c24f23
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2022-10-25 09:43:50 +0900

    [Gold IV] Title: 알고스팟, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+using namespace std;
+
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int visited[102][102];
+char board[102][102];
+
+struct node {
+	int x, y, c;
+};
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int N, M;
+	cin >> M >> N;
+	
+	for(int i=1; i<=N; i++) {
+		for(int j=1; j<=M; j++) {
+			cin >> board[i][j];
+		}
+	}
+	
+	deque<node> d;
+	d.push_back({1,1,0});
+	
+	while(!d.empty()) {
+		node cnode = d.front();
+		d.pop_front();
+		
+		if(cnode.x==N && cnode.y==M) {
+			cout << cnode.c;
+			return 0;
+		}
+		
+		for(int i=0; i<4; i++) {
+			int nx = cnode.x+dx[i];
+			int ny = cnode.y+dy[i];
+			
+			if(nx>0 && nx<=N && ny>0 && ny<=M) {
+				if(visited[nx][ny]==0) {
+					visited[nx][ny]=1;
+					
+					if(board[nx][ny]=='1') {
+						d.push_back({nx,ny,cnode.c+1});
+					}
+					else {
+						d.push_front({nx,ny,cnode.c});
+					}
+				}
+			}
+		}
+	}
+	
+	return 0;
+}
