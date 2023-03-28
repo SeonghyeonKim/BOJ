d0d57d1dc563fbe065f86eca7c47e3057b1fd548
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2023-03-28 10:57:05 +0900

    [Bronze V] Title: Serca, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin >> n;
+	
+	while(n--) {
+		cout << " @@@   @@@ \n\
+@   @ @   @\n\
+@    @    @\n\
+@         @\n\
+ @       @ \n\
+  @     @  \n\
+   @   @   \n\
+    @ @    \n\
+     @     \n";
+	}
+	
+		
+	return 0;
+}
