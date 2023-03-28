fde89ae949e2240264d6a2aaebe3c490968d88d3
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2023-03-28 11:03:49 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int a, b;
+	cin >> a >> b;
+	cout << a*10+b;	
+		
+	return 0;
+}
