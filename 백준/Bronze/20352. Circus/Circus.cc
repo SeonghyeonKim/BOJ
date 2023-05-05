58d9bec74d23a7b38c3aaf42935245682d7987b8
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2023-05-06 00:13:27 +0900

    [Bronze IV] Title: Circus, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20352. Circus/Circus.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main() {
+	double a;
+	cin >> a;
+	
+	cout.precision(7);
+	cout << fixed;
+	cout << sqrt(a/3.1415926535)*2*3.1415926535;
+    		 
+    return 0;
+}
