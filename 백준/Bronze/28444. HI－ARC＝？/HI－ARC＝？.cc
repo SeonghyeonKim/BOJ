ddb490a8227c42fdd67ae8b146192e10d2771d7a
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2023-08-14 15:46:24 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b, c, d, e;
+	cin >> a >> b >> c >> d >> e;
+	
+	cout << a*b - c*d*e;	
+
+    return 0;
+}
+
