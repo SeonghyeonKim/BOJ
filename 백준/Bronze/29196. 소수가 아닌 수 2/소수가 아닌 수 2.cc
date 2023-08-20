7ec83977e9e4be7f66640c2f9cda24d60f163e0e
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2023-08-20 17:26:14 +0900

    [Bronze I] Title: 소수가 아닌 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29196. 소수가 아닌 수 2/소수가 아닌 수 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	// cin.tie(NULL);
+	// ios_base::sync_with_stdio(false);
+
+	double s;
+	cin >> s;
+	
+	cout << "YES\n";
+	cout << int(s*1000000000) << " 1000000000";
+
+    return 0;
+}
