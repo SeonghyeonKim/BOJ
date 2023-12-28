f13f47997ec296729e20784036eb60111950129a
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2023-12-28 14:58:41 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	
+	for(int i=0; i<5*N; i++) {
+		if(i < 5*N - N) {
+			for(int j=0; j<N; j++) {
+				cout << "@";
+			}
+			
+			cout << "\n";
+			continue;
+		}
+		
+		for(int j=0; j<5*N; j++) {
+			cout << "@";	
+		}
+		cout << "\n";
+	}
+   
+    return 0;
+}
