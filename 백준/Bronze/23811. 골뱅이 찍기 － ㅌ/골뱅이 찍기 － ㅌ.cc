1fc45f4367f368ab27eae56daf35e1a463de0f1a
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-01-17 14:48:58 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	
+	for(int i=0; i<5*N; i++) {
+		if((i>=N && i<2*N) || (i>=3*N && i<4*N)) {
+			for(int j=0; j<N; j++) {
+				cout << "@";
+			}
+		}
+		else {
+			for(int j=0; j<5*N; j++) {
+				cout << "@";
+			}
+		}
+		cout << "\n";
+	}
+		
+	return 0;
+}
