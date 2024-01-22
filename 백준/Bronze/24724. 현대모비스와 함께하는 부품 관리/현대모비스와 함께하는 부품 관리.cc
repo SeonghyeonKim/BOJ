bb2a96c5911b855f637b47c14471bb965b96c5d2
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-01-22 11:31:06 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int T;
+	cin >> T;
+	
+	for(int i=1; i<=T; i++) {
+		int N;
+		cin >> N;
+		
+		for(int i=0; i<=N; i++) {
+			int a, b;
+			cin >> a >> b;
+		}
+		cout << "Material Management " << i << "\nClassification ---- End!\n";
+	}
+	
+	return 0;
+}
