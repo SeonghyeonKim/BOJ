be241f79190813f891dd1bec93e77acbdbe1a142
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-01-22 11:47:06 +0900

    [Bronze III] Title: Not A + B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <stack>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+	
+	for(int i=0; i<N; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << "1\n";
+	}
+	
+	return 0;
+}
