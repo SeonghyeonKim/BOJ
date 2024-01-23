4fec59a5a8b3068e5f1a74345b29366088050bce
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-01-23 14:02:18 +0900

    [Bronze III] Title: Dominos, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15236. Dominos/Dominos.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+ 	cout << (n+1)*(n+2)/2 * n;
+ 
+	return 0;
+}
