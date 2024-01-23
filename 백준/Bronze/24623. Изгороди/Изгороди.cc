ae6f81ebac0467a481c6fb75dc6c09da61a8ac72
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-01-23 15:47:15 +0900

    [Bronze III] Title: Изгороди, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24623. Изгороди/Изгороди.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b;
+	cin >> a >> b;
+	cout << 90;
+	 
+	return 0;
+}
