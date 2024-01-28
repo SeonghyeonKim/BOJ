9d917e721e5bf1355d18e4e561b39795e8edccca
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-01-29 02:36:37 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+	string a, b;
+	cin >> a >> b;
+	cout << b;
+	
+    return 0;
+}
