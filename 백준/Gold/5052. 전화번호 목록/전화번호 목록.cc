c39a205ef54ee5d45f340293d3a3e9cca2772142
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-02-27 23:10:27 +0900

    [Gold IV] Title: 전화번호 목록, Time: 56 ms, Memory: 2384 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -4,47 +4,6 @@
 #include <algorithm>
 using namespace std;
 
-struct TRIE{
-	bool Finish;
-	
-	TRIE *Node[10];
-	
-	TRIE() {
-		Finish = false;
-		for(int i=0; i<10; i++) Node[i] = NULL;
-	}	
-	
-	~TRIE() {
-		for(int i=0; i<10; i++) {
-			if(Node[i]) delete Node[i];
-		}
-	} 
-
-	void Insert(string s) {
-		if(s == "") {
-			Finish = true;
-			return ;
-		}
-		
-		int Cur = s[0] - '0';
-		if(Node[Cur] == NULL) Node[Cur] = new TRIE();
-		Node[Cur]->Insert(s.substr(1)); 
-	} 
-	
-	bool Find(string s) {
-		if(s == "") {
-			for(int i=0; i<10; i++) {
-				if(Node[i] != NULL) return true;
-			}
-			return false;
-		}
-		
-		int Cur = s[0] - '0';
-		if(Node[Cur] == NULL) return false;
-		return Node[Cur]->Find(s.substr(1));
-	} 
-};
-
 int main() {
 	cin.tie(NULL);
     ios_base::sync_with_stdio(false);
@@ -56,17 +15,15 @@ int main() {
 		int N, flag=0;
 		cin >> N;
 		
-		TRIE *root = new TRIE();
 		vector<string> v(N);
 		
 		for(int i=0; i<N; i++) 
 			cin >> v[i];
 			
-		sort(v.begin(), v.end(), greater<string> ());
+		sort(v.begin(), v.end());
 		
-		for(int i=0; i<N; i++) {
-			if(root->Find(v[i])) flag = 1;	
-			root->Insert(v[i]);	
+		for(int i=0; i<N-1; i++) {
+			if(v[i] == v[i+1].substr(0, v[i].size())) flag=1;
 		}
 		
 		if(flag) cout << "NO\n";
