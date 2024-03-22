d80280a19bf63ac94c0bc34baa78ef90b1cbef44
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-03-22 18:10:12 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,30 +1,40 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
+
+	int A, B, ans=1;
+	cin >> A >> B;
 	
-    int A, B;
-    cin >> A >> B;
-
-    queue<pair<long long, long long> > q;
-    q.push({A, 0});
-	
-    while(!q.empty()) {
-		long long num=q.front().first, cnt=q.front().second;
-		q.pop();
-
-        if(num==B) {
-            cout << cnt+1;
-            exit(0);
-        }
-
-		if(num*2<=B) q.push({num*2, cnt+1});
-		if(num*10+1<=B) q.push({num*10+1, cnt+1});
+	while(1) {
+		if(A > B) {
+			cout << -1;
+			break;
+		}
+		
+		if(A == B) {
+			cout << ans;
+			break;
+		}
+		
+		if(B%10 == 1) {
+			B/=10;
+		} 
+		else if(B%2==0) {
+			B/=2;
+		}
+		else {
+			cout << -1;
+			break;
+		}
+		ans++;
 	}
-    cout << -1;
-
+	
 	return 0;
 }
+
