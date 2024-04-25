41871774e2eeae3a6500e4dc676662f60e72a7ea
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-04-25 16:19:09 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#define MOD 1000000007
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	int a;
+	cin >> a;
+	cout << a/11*10;
+	
+	return 0;
+}
