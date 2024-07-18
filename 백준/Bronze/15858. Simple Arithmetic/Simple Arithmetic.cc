662b9b4cc473b1bbe3ee070d39a6ee65b0634132
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-07-18 16:06:07 +0900

    [Bronze III] Title: Simple Arithmetic, Time: 0 ms, Memory: 2020 KB, Score: 75 point -BaekjoonHub

--- 백준/Bronze/15858. Simple Arithmetic/Simple Arithmetic.cc
+++ 백준/Bronze/15858. Simple Arithmetic/Simple Arithmetic.cc
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 int main(void) {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
-	long long a, b, c;
-	cin >> a >> b >> c;
-	cout.precision(7);
-	cout << fixed;
-	cout << a*b*1.0/c;
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
 	
-    return 0;
+	int a, b, c;
+	cin >> a >> b >> c;
+	
+	cout.precision(8);
+	cout << fixed;
+	
+	long double ans = (long double)a/c;
+	cout << ans * (long double)b;
+	
+	 
+	return 0;
 }
