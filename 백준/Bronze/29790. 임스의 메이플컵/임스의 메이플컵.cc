09e0ca72fa860e85d306d67f56ab850abb796142
SeonghyeonKim <48244988+SeonghyeonKim@users.noreply.github.com>
2024-08-07 08:16:14 +0900

    [Bronze IV] Title: 임스의 메이플컵, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29790. 임스의 메이플컵/임스의 메이플컵.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+	int N, U, L;
+	cin >> N >> U >> L;
+	
+	if(N>=1000 && (U>=8000 || L>=260)) cout << "Very Good";
+	else if(N>=1000) cout << "Good";
+	else cout << "Bad";
+	    
+    return 0;
+}
